99f6e39514bb494132281a120b5804c33295d00d
Samuel Rubin <sjrube32@gmail.com>
2022-03-16 13:53:55 -0400

    More comments and added some more todos for myself

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -25,7 +25,9 @@ public class PlayerMovement : MonoBehaviour
     private DistanceJoint2D distanceJoint;
     private SpriteRenderer spriteRenderer;
 
-    // This is the Layer within unity that all the ground will be apart of, and what we consider to be counted as ground.
+    /// <summary>
+    /// This is the Layer within unity that all the ground will be apart of, and what we consider to be counted as ground.
+    /// </summary>
     public LayerMask jumpableGround;
 
     /// <summary>
@@ -35,6 +37,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private bool withinHookRadius;
 
+
+    //Sprite Variables, used for testing purposes. Will have to encorperate something later but will almost certainly be removed or changed.
+    // TODO: Look into this.
     public Sprite baseSprite;
     public Sprite secondSprite;
 
@@ -44,17 +49,18 @@ public class PlayerMovement : MonoBehaviour
         rigidBody = this.GetComponent<Rigidbody2D>();
         capsuleCollider = this.GetComponent<CapsuleCollider2D>();
         distanceJoint = this.GetComponent<DistanceJoint2D>();
-
         spriteRenderer = this.GetComponent<SpriteRenderer>();
 
         //Sets the anchor point to us then turns the Joint off.
         distanceJoint.enabled = false;
-        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        
+        // TODO: add condition that input is only taken when we are not actively hooking.
+        
         //Gets Input from unity horizontal axis. Raw determines that it is always only -1, 0, or 1.
         float dirX = Input.GetAxisRaw("Horizontal");
 
@@ -87,8 +93,6 @@ public class PlayerMovement : MonoBehaviour
 
             // TODO: Note that the above Debug is called every frame this condition is met to be true, add here some form of duration control that limits how long
             // a user can be hooked for.
-
-
         }
         // If any of the condition is not met, meaning we are not attempting to swing. We turn the DistanceJoint2D off.
         else
@@ -111,7 +115,6 @@ public class PlayerMovement : MonoBehaviour
     /// Casts a box directly under the player character, then checks for if that box collides with anything from jumpableGround (currently set to Layer: Ground).
     /// </summary>
     /// <returns>returns true if a collision happens (meaning we are touching the ground), false if there is no collision.</returns>
-
     private bool isGrounded()
     {
         return Physics2D.BoxCast(capsuleCollider.bounds.center, capsuleCollider.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
