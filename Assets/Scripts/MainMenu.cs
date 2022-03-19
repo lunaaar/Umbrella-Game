b3cf4b889fae8e0551776009d1b894d65950dc8a
Creem <creema@wit.edu>
2022-03-19 12:27:37 -0400

    Main menu implementation

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Runs on play button click
+    public void PlayGame()
+    {
+        //main menu is 0 in build index, level 1 is 1
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Quit game - This is not visible in unity editor so there is a debug message
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
