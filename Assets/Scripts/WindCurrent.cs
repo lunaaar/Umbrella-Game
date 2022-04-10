be168a5fb4b796bc11451e5c8ab32ba65ca6f834
Samuel Rubin <sjrube32@gmail.com>
2022-04-10 17:25:38 -0400

    Level Design stuff

--- Assets/Scripts/WindCurrent.cs
+++ Assets/Scripts/WindCurrent.cs
@@ -11,7 +11,7 @@ public class WindCurrent : MonoBehaviour
         if (collision.tag == "Player" && Input.GetKey(KeyCode.Space))
         {
             Vector2 force = new Vector2(0, Mathf.Cos(Mathf.Abs(this.transform.rotation.eulerAngles.z)));
-            collision.GetComponent<PlayerMovement>().rigidBody.AddForce( force * 12, ForceMode2D.Force);
+            collision.GetComponent<PlayerMovement>().rigidBody.AddForce( force * speed, ForceMode2D.Force);
         }
     }
 }
